1bf7f496b64d80ce69726a018dce534c7fbab416
ItsWazzaP <159933355+ItsWazzaP@users.noreply.github.com>
2025-06-07 13:41:56 +0200

    Factorial Calculator
    Took me 47 minutes to figure this out 😭
    Apparently it works well to use the for loop "i" in calculations. Good to know ^_^
    
    Also moved all C files from School into /School/2nd Year/

---
+++ Home/Independent/Factorial.c
@@ -0,0 +1,26 @@
+// I'll try to make a factorial calculator using a FOR-loop
+
+#include <stdio.h>
+
+int main()
+{
+    int number, factorial;
+
+    printf("Enter the number you want to calculate the factorial for: ");
+    scanf("%d", &number);
+
+    factorial = 1;
+
+    if (number >= 0)
+    {
+        for (int i = 1; i <= number; i++) // Set i to 1. For each i that is less than number, increment i by 1
+        {
+            factorial *= i; // I tried to multiply this with "number", which messed it up. Just use the for loop "i"
+        }
+        printf("The factorial of %d is %d\n", number, factorial);
+    }
+    else
+    {
+        printf("Entered number (%d) isn't valid", number);
+    }
+}
