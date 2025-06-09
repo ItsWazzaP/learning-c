49fda75ff66c3981b4180ddb2d29c600f672ba5f
ItsWazzaP <159933355+ItsWazzaP@users.noreply.github.com>
2025-06-09 13:25:50 +0200

    More Surface Calculator additions
    Made an ExitProgram function since it's used in many places
    
    Added IF check to Circle
    
    Added more 2D body options
    
    Finished Parallelogram
    Started Rhombus

--- Home/Independent/Surface Calculator.c
+++ Home/Independent/Surface Calculator.c
@@ -30,8 +30,14 @@ REMINDERS:
 %c - char
 %s - string (array of characters)
 ********************************/
+void ExitProgram() // Way too many places where this needed to be used, so I finally made it a separate function I can call
+{
+    printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
+    system("pause");
+    exit(EXIT_FAILURE);
+}
 
-void Square()
+void Square() /*••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••*/
 {
     double square_side, square_diameter, square_surface;
     int square_choice;
@@ -45,10 +51,8 @@ void Square()
             printf("\nEnter your square's side: ");
             scanf("%lf", &square_side);
             /////
-            if (square_side <= 0) {// Closes the program if the user is dumb :}
-                printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                system("pause"); // Pause so the user can get to know they're dumb D:
-                exit(EXIT_FAILURE); }
+            if (square_side <= 0) {
+                ExitProgram(); }
             /////
             break;
         case 2: // If Diameter
@@ -56,28 +60,19 @@ void Square()
             scanf("%lf", &square_diameter);
             /////
             if (square_diameter <= 0) {
-                printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                system("pause");
-                exit(EXIT_FAILURE); }
+                ExitProgram(); }
             /////
             square_side = square_diameter / sqrt(2);
             break;
         default:
-            printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-            system("pause");
-            exit(EXIT_FAILURE);
+            ExitProgram();
             break;
     }
 
     square_surface = pow(square_side, 2); // Raising square_side to the power of 2 (notes for myself in case I forget how it's read)
     printf("The surface of your square is %lf\n\n", square_surface);
 }
-
-
-////////////////////////////////////////////////////////
-
-
-void Rectangle()
+void Rectangle() /*••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••*/
 {
     double rectangle_base, rectangle_height, rectangle_diameter, rectangle_angle, rectangle_surface;
     int rectangle_choice;
@@ -94,9 +89,7 @@ void Rectangle()
             scanf("%lf", &rectangle_height);
             /////
             if (rectangle_base <= 0 || rectangle_height <= 0) {
-                printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                system("pause");
-                exit(EXIT_FAILURE); }
+                ExitProgram(); }
             /////
             rectangle_surface = rectangle_base * rectangle_height;
             break;
@@ -107,9 +100,7 @@ void Rectangle()
             scanf("%lf", &rectangle_angle);
             /////
             if (rectangle_diameter <= 0 || rectangle_angle <= 0 || rectangle_angle >= 90 || rectangle_angle <= 0) {
-                printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                system("pause");
-                exit(EXIT_FAILURE); }
+                ExitProgram(); }
             /////
             rectangle_angle = rectangle_angle * (PI / 180); // Converting to radians
 
@@ -119,71 +110,55 @@ void Rectangle()
             rectangle_surface = rectangle_base * rectangle_height;
             break;
         default:
-            printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-            system("pause");
-            exit(EXIT_FAILURE);
+            ExitProgram();
             break;
     }
 
     printf("The surface of your rectangle is %lf\n\n", rectangle_surface);
 }
-
-
-////////////////////////////////////////////////////////
-
-
-void Circle()
+void Circle() /********************************************************/
 {
     // Won't ask for the diameter, since radius = diameter / 2. Simple enough
     double circle_radius, circle_surface;
 
     printf("Enter your circle's radius (r): ");
     scanf("%lf", &circle_radius);
-
+    /////
+    if (circle_radius <= 0) {
+        ExitProgram(); }
+    /////
     circle_surface = PI * pow(circle_radius, 2);
 
     printf("The surface of your circle is %lf\n\n", circle_surface);
 }
-
-
-////////////////////////////////////////////////////////
-
-
-void Triangle()
+void Triangle() /*••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••*/
 {
     /*
         Types of triangles and their surface (area) formulas:
 
         * By sides:
-
         - Scalene (0 equal sides)
-            S = (base * height) / 2                // Any triangle if you know height
-
+            triangle_surface = (base * height) / 2                          // Any triangle if you know height
         - Isosceles (2 equal sides)
-            S = (base * height) / 2                // Still requires perpendicular height
-
+            triangle_surface = (base * height) / 2                          // Still requires perpendicular height
         - Equilateral (3 equal sides)
-            S = (pow(side, 2) * sqrt(3)) / 4       // Only works for equilateral
+            triangle_surface = (pow(side, 2) * sqrt(3)) / 4                 // Only works for equilateral
 
         * By angles:
-
         - Acute (all angles < 90°)
-            S = (base * height) / 2                // Same general rule applies
-
+            triangle_surface = (base * height) / 2                          // Same general rule applies
         - Right (1 angle = 90°)
-            S = (side_a * side_b) / 2              // a and b are the two legs (not the hypotenuse)
-
+            triangle_surface = (side_a * side_b) / 2                        // a and b are the two legs (not the hypotenuse)
         - Obtuse (1 angle > 90°)
-            S = (base * height) / 2                // Height must be from the obtuse angle’s vertex
+            triangle_surface = (base * height) / 2                          // Height must be from the obtuse angle’s vertex
 
         * Trigonometric formulas:
-
          When you know two sides and the included angle (SAS):
-            S = (side_a * side_b * sin(angle_c)) / 2         // angle_c is between sides a and b
+            triangle_surface = (side_a * side_b * sin(angle_c)) / 2         // angle_c is between sides a and b
 
         * Heron’s Formula (when you know all 3 sides):
-            s = (side_a + side_b + hypotenuse) / 2                    // semi-perimeter
-            S = sqrt(s * (s - side_a) * (s - side_b) * (s - hypotenuse))
+            heron_s = (side_a + side_b + hypotenuse) / 2                    // semi-perimeter
+            triangle_surface = sqrt(s * (s - side_a) * (s - side_b) * (s - hypotenuse))
     */
     double side, side_a, side_b, hypotenuse, base, angle_a, angle_b, angle_c, height, heron_s, triangle_surface;
     int triangle_choice, know_all_triangle_sides;
@@ -204,9 +179,7 @@ void Triangle()
             scanf("%lf", &hypotenuse);
             /////
             if (side_a <= 0 || side_b <= 0 || hypotenuse <= 0) {
-                printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                system("pause");
-                exit(EXIT_FAILURE); }
+                ExitProgram(); }
             /////
             heron_s = (side_a + side_b + hypotenuse) / 2;
             triangle_surface = sqrt(heron_s * (heron_s - side_a) * (heron_s - side_b) * (heron_s - hypotenuse));
@@ -223,9 +196,7 @@ void Triangle()
                     scanf("%lf", &height);
                     /////
                     if (base <= 0 || height <= 0) {
-                        printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                        system("pause");
-                        exit(EXIT_FAILURE); }
+                        ExitProgram(); }
                     /////
                     triangle_surface = (base * height) / 2;
 
@@ -239,9 +210,7 @@ void Triangle()
                     scanf("%lf", &height);
                     /////
                     if (base <= 0 || height <= 0) {
-                        printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                        system("pause");
-                        exit(EXIT_FAILURE); }
+                        ExitProgram(); }
                     /////
                     triangle_surface = (base * height) / 2;
                     
@@ -253,9 +222,7 @@ void Triangle()
                     scanf("%lf", &side);
                     /////
                     if (side <= 0) {
-                        printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                        system("pause");
-                        exit(EXIT_FAILURE); }
+                        ExitProgram(); }
                     /////
                     triangle_surface = (pow(side, 2) * sqrt(3)) / 4;
 
@@ -271,9 +238,7 @@ void Triangle()
                     scanf("%lf", &height);
                     /////
                     if (base <= 0 || height <= 0) {
-                        printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                        system("pause");
-                        exit(EXIT_FAILURE); }
+                        ExitProgram(); }
                     /////
                     triangle_surface = (base * height) / 2;
                     
@@ -287,9 +252,7 @@ void Triangle()
                     scanf("%lf", &side_b);
                     /////
                     if (side_a <= 0 || side_b <= 0) {
-                        printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                        system("pause");
-                        exit(EXIT_FAILURE); }
+                        ExitProgram(); }
                     /////
                     triangle_surface = (side_a * side_b) / 2;
 
@@ -303,9 +266,7 @@ void Triangle()
                     scanf("%lf", &height);
                     /////
                     if (base <= 0 || height <= 0) {
-                        printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                        system("pause");
-                        exit(EXIT_FAILURE); }
+                        ExitProgram(); }
                     /////
                     triangle_surface = (base * height) / 2;
                     
@@ -313,9 +274,7 @@ void Triangle()
                     break;
 
                 default:
-                    printf("\nYou have entered an incorrect value!\nThe values are listed above (1-6). Exiting the program...\n\n");
-                    system("pause");
-                    exit(EXIT_FAILURE);
+                    ExitProgram();
                     break;
             }
             break;
@@ -328,9 +287,7 @@ void Triangle()
             scanf("%lf", &angle_c);
             /////
             if (side_a <= 0 || side_b <= 0 || angle_c <= 0 || angle_c >= 180) {
-                printf("\nYou have entered an incorrect value! Exiting the program...\n\n");
-                system("pause");
-                exit(EXIT_FAILURE); }
+                ExitProgram(); }
             /////
             angle_c = angle_c * (PI / 180);
             triangle_surface = (side_a * side_b * sin(angle_c)) / 2;
@@ -338,11 +295,125 @@ void Triangle()
             printf("The surface of your triangle is %lf\n\n", triangle_surface);
             break;
         default:
-            printf("\nYou have entered an incorrect value!\nThe values are listed above (1-3). Exiting the program...\n\n");
-            system("pause");
-            exit(EXIT_FAILURE);
+            ExitProgram();
             break;
     }
+}
+void Parallelogram() /*••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••*/
+{
+    double base, side, height, long_diagonal, short_diagonal, angle, parallelogram_surface;
+    int parallelogram_choice;
+
+    printf("\nWhat do you know about your parallelogram?\n1 - Base & height\n2 - Two diagonals and an angle\n3 - Base & side and an angle");
+    scanf("%d", &parallelogram_choice);
+    
+    switch (parallelogram_choice)
+    {
+        case 1:
+            printf("Enter your parallelogram's base: ");
+            scanf("%lf", &base);
+            printf("Enter your parallelogram's height: ");
+            scanf("%lf", &height);
+            /////
+            if (base <= 0 || height <= 0) {
+                ExitProgram(); }
+            /////
+            parallelogram_surface = base * height;
+
+            printf("The surface of your parallelogram is %lf\n\n", parallelogram_surface);
+            break;
+        case 2:
+            printf("Enter your parallelogram's long diagonal: ");
+            scanf("%lf", &long_diagonal);
+            printf("Enter your short diagonal: ");
+            scanf("%lf", &short_diagonal);
+            printf("Enter your angle (one of the angles where the diagonals meet): ");
+            scanf("%lf", &angle);
+            /////
+            if (long_diagonal <= 0 || short_diagonal <= 0 || angle >= 180 || angle <= 0) {
+                ExitProgram(); }
+            /////
+            angle = angle * (PI / 180);
+            parallelogram_surface = (long_diagonal * short_diagonal * sin(angle));
+
+            printf("The surface of your parallelogram is %lf\n\n", parallelogram_surface);
+            break;
+        case 3:
+            printf("Enter your parallelogram's base: ");
+            scanf("%lf", &base);
+            printf("Enter your parallelogram's side: ");
+            scanf("%lf", &side);
+            printf("Enter your parallelogram's angle (DAB or BCD): ");
+            scanf("%lf", &angle);
+            /////
+            if (base <= 0 || side <= 0 || angle >= 180 || angle <= 0) {
+                ExitProgram(); }
+            /////
+            angle = angle * (PI / 180);
+            parallelogram_surface = base * side * sin(angle);
+
+            printf("The surface of your parallelogram is %lf\n\n", parallelogram_surface);
+            break;
+        default:
+            ExitProgram();
+            break;
+    }
+}
+void Rhombus() /*••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••*/
+{
+    double rhombus_side, rhombus_height, rhombus_first_diagonal, rhombus_second_diagonal, angle, rhombus_surface;
+    int rhombus_choice;
+
+    printf("\nWhat do you know about your parallelogram?\n1 - Side & height\n2 - Diagonals\n3 - Side & angle");
+    scanf("%d", &rhombus_choice);
+
+    switch (rhombus_choice)
+    {
+        case 1:
+            printf("Enter your rhombus' side: ");
+            scanf("%lf", &rhombus_side);
+            printf("Enter your rhombus' height: ");
+            scanf("%lf", &rhombus_height);
+            /////
+            if (rhombus_side <= 0 || rhombus_height <= 0) {
+                ExitProgram(); }
+            /////
+            rhombus_surface = rhombus_side * rhombus_height;
+
+            printf("The surface of your rhombus is %lf\n\n", rhombus_surface);
+            break;
+        case 2:
+            printf("Enter your rhombus' first diagonal: ");
+            scanf("%lf", &rhombus_first_diagonal);
+            printf("Enter your rhombus' first diagonal: ");
+            scanf("%lf", &rhombus_second_diagonal);
+            break;
+        case 3:
+            break;
+        default:
+            ExitProgram();
+            break;
+    }
+}
+void Deltoid() /*••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••*/
+{
+    double side_a, side_b, deltoid_surface;
+}
+void Trapezoid()
+{
+    double trapezoid_surface;
+}
+void Pentagon() /*••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••*/
+{
+    double pentagon_surface;
+}
+void Hexagon() /*••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••*/
+{
+    double hexagon_surface;
+}
+void NGon() /*••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••*/
+{
+    double ngon_surface;
 }
 
 ////////////////////////////////////////////////////////
@@ -353,8 +424,9 @@ int main()
 {
     int body;
 
-    printf("Please use the metric system instead of the imperial for measurements!\n\n");
-    printf("1 - Square\n2 - Rectangle\n3 - Circle\n4 - Triangle\nEnter what type of body you wanna calculate the surface for: ");
+    printf("Please use the metric system instead of the imperial for measurements!\nInput angles in degrees, not radians!\n\n");
+    printf("1 - Square\n2 - Rectangle\n3 - Circle\n4 - Triangle\n5 - Parallelogram\n6 - Rhombus\n7 - Deltoid\n8 - Pentagon\n9 - Hexagon\n10 - N-gon");
+    printf("\nEnter what type of body you wanna calculate the surface for: ");
     scanf("%d", &body);
 
     switch (body)
@@ -371,8 +443,29 @@ int main()
         case 4:
             Triangle();
             break;
+        case 5:
+            Parallelogram();
+            break;
+        case 6:
+            Rhombus();
+            break;
+        case 7:
+            Deltoid();
+            break;
+        case 8:
+            Trapezoid();
+            break;
+        case 9:
+            Pentagon();
+            break;
+        case 10:
+            Hexagon();
+            break;
+        case 11:
+            NGon();
+            break;
         default:
-            printf("\nYou have entered an incorrect value!\nThe values are listed above (1-4). Exiting the program...\n\n");
+            ExitProgram();
             break;        
     }
 
