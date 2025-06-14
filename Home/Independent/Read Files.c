3b9964444bb53bc500a2cf68aee5393582b6960f
ItsWazzaP <159933355+ItsWazzaP@users.noreply.github.com>
2025-06-14 15:07:04 +0200

    Finished Read Files
    Finally finished "Read Files", and now I can dynamically calculate my grades much easier.
    Just open Subjects.txt, edit a few things, save, run the program and all the other stuff is done for me! :D

--- Home/Independent/Read Files.c
+++ Home/Independent/Read Files.c
@@ -5,11 +5,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    FILE *parse_file = fopen("Subjects.txt", "r"); // Needs to be a pointer variable
-    char subjects[255]; // Buffer - holds 1 line of the text document at a time. Set to 30 bytes as a minimum for this case
+    float grade_arithmetic_mean, grade, number_of_subjects = 0, grade_sum = 0;
+
+    FILE *parse_file = fopen("Subjects.txt", "r"); // Needs to be a pointer variable. fopen() is file open
+    char subjects[255]; // Buffer - holds 1 line of the text document at a time
 
     if (parse_file == NULL) // Checking if the file exists. If it doesn't, pointer returns NULL
     {
@@ -18,16 +21,42 @@ int main()
         exit(EXIT_FAILURE);
     }
     else
-    {
-        while(fgets(subjects, 255, parse_file) != NULL) // Have to move fgets() into the loop, since after it's finished it returns NULL
+    { // sizeof() something in bytes, 
+        while(fgets(subjects, sizeof(subjects), parse_file) != NULL) // Have to move fgets() into the loop, since after it's finished it returns NULL
         {
-            //fgets(subjects, 30, parse_file); // Read a single file 
-            printf("%s", subjects);
+            char *colon = strrchr(subjects, ':'); // Find the ':' and leave the grade (String Reverse Char Search). Searches for the last occurence of that char
+            
+            if (colon != NULL)
+            {
+                grade = atof(colon + 1); // The "+ 1" tells it to go for " [grade_number]". atof() is ASCII to float
+                grade_sum += grade; // Adds the grade as soon as it's detected to the total sum
+                number_of_subjects += 1; // Basically a line counter
+
+                printf("Subject: %-35.*s Grade: %.0f\n", (int)(colon - subjects), subjects, grade);
+                // colon_ptr - subjects --- length of subject name before ':'
+                //        (int)         --- cast to int for printing
+                //       subjects       --- pointer to a subject name
+                //        grade         --- number after the colon
+                //       %-35.*s        --- idk i found this it just makes it look like a table
+            }
+            else
+            {
+                printf("Missing colon (':')!");
+            }
+
+            //printf("%s", subjects);
         }
     }
 
-    fclose(parse_file); // Close any file - FILE variable, no pointer here
+    fclose(parse_file); // Close any file - FILE variable, no pointer here. fclose() is file close
+
+    grade_arithmetic_mean = grade_sum / number_of_subjects;
+
+    printf("\n\nYour grade's arithmetic mean is: %.2f\n\n", grade_arithmetic_mean);
+
     system("pause");
 
     return 0;
 }
+
+// This was enjoyable. I think I'm gonna make more programs like this
